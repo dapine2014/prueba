076f2bd203ed648bd1a16ac1f762bb7dd6bbdcf0
alex <dapine@gmail.com>
2024-01-17 10:54:38 -0500

    se ajusto el Dto

--- src/main/java/com/fonyou/prueba/prueba/application/dto/PruebaDto.java
+++ src/main/java/com/fonyou/prueba/prueba/application/dto/PruebaDto.java
@@ -1,16 +1,23 @@
 package com.fonyou.prueba.prueba.application.dto;
 
-import lombok.Value;
+import lombok.*;
 
 import java.io.Serializable;
 
 /**
  * DTO for {@link com.fonyou.prueba.prueba.model.Prueba}
  */
-@Value
-public class PruebaDto implements Serializable {
-    Long id;
-    String nombre;
-    String objetivo;
-    String comentario;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PruebaDto {
+    private Long id;
+
+    @NonNull
+    private String nombre;
+
+    private String objetivo;
+
+    private String comentario;
 }
