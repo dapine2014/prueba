1f0122034e78d6981b43f8fab2e31c884745b262
alex <dapine@gmail.com>
2024-01-17 23:06:33 -0500

    ajuste del caso de uso de crud

--- src/main/java/com/fonyou/prueba/prueba/application/usercase/CrudCaseImpl.java
+++ src/main/java/com/fonyou/prueba/prueba/application/usercase/CrudCaseImpl.java
@@ -1,37 +1,57 @@
 package com.fonyou.prueba.prueba.application.usercase;
 
 import com.fonyou.prueba.prueba.application.dto.PruebaDto;
+import com.fonyou.prueba.prueba.infraestructure.ports.input.repository.PruebaRepository;
 import com.fonyou.prueba.prueba.infraestructure.ports.input.service.ICrudCaseService;
+import com.fonyou.prueba.prueba.model.Prueba;
+import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CrudCaseImpl implements ICrudCaseService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CrudCaseImpl.class);
 
+    @Autowired
+    private PruebaRepository repository;
 
+    private final ModelMapper mapper = new ModelMapper();
     @Override
     public PruebaDto saveUpdate(PruebaDto prueba) {
-        return null;
+        LOGGER.info("## START SAVE PRUEBA ##");
+        Object response = repository.saveAndFlush(mapper.map(prueba, Prueba.class));
+        LOGGER.info("## SAVED PRUEBA ##");
+
+        return mapper.map(response, PruebaDto.class);
     }
 
     @Override
     public List<PruebaDto> showAll() {
-        return null;
+        LOGGER.info("## START READ PRUEBAS ##");
+        List<PruebaDto> pruebas = repository.findAll().stream().map(estudiante -> mapper.map(estudiante, PruebaDto.class)).collect(Collectors.toList());
+        LOGGER.info("## END OF READING PRUEBAS ##");
+
+        return pruebas;
     }
 
     @Override
     public Optional<PruebaDto> showByID(Long id) {
-        return Optional.empty();
+        LOGGER.info("## START SEARCH PRUEBA ##");
+        Optional<Prueba> response =  repository.findById(id);
+        LOGGER.info("## END SEARCH PRUEBA ##");
+
+        return response.map((element) -> mapper.map(element, PruebaDto.class));
     }
 
     @Override
     public void deletePrueba(Long id) {
-
+        repository.deleteById(id);
     }
 }
--- src/main/java/com/fonyou/prueba/prueba/utils/Constant.java
+++ src/main/java/com/fonyou/prueba/prueba/utils/Constant.java
@@ -6,7 +6,7 @@ public class Constant {
   public static  int SUCCES_ERROR = 500;
   public static  String RESPONSE_OK = "Responce ok";
 
-  public static String DELETE_OK = "ESTUDIANTE BORRADO CORRECTAMENTE";
+  public static String DELETE_OK = "DATO BORRADO CORRECTAMENTE";
 
   public static  String RESPONSE_NOT_OK = "NO HAY DATOS PARA LA CONSULTA";
 
